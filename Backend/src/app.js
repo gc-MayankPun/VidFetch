429bd4a177dd5eb0a336c9f3fdb495684ebd77ce
gc-MayankPun <punmayank35@gmail.com>
2026-03-19 15:25:38 +0530

    test fetch

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -33,6 +33,28 @@ app.get("/test-ytdlp", async (req, res) => {
   }
 });
 
+app.get("/test-ytdlp", async (req, res) => {
+  const { spawn } = await import("child_process");
+  const chunks = [];
+  const proc = spawn("/usr/local/bin/yt-dlp", [
+    "--dump-json",
+    "--no-playlist",
+    "--socket-timeout", "30",
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  ]);
+  proc.stdout.on("data", d => chunks.push(d));
+  proc.stderr.on("data", d => console.error("stderr:", d.toString()));
+  proc.on("close", code => {
+    if (code === 0) {
+      const info = JSON.parse(Buffer.concat(chunks).toString());
+      res.json({ ok: true, title: info.title });
+    } else {
+      res.json({ ok: false, code });
+    }
+  });
+  setTimeout(() => { proc.kill(); res.json({ ok: false, error: "timed out" }); }, 30000);
+});
+
 app.use("/{*path}", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
